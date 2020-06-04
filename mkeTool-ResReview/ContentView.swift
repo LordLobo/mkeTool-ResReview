5e19e840c5d30bd5892824ead7dc592d73e1e921
Dan Giralte <dangiralte@blinkupapp.com>
2020-06-04 17:28:17 -0500

    Initial Commit

---
+++ mkeTool-ResReview/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  mkeTool-ResReview
+//
+//  Created by Dan Giralte on 6/4/20.
+//  Copyright © 2020 Dan Giralte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
